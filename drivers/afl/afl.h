66687b6eda8bd0342eeec5ac8f4adde9f31236bc
Quentin Casasnovas <quentin.casasnovas@oracle.com>
2015-10-31 01:09:04 +0100

    afl: for mmap at different offsets for each open instance.
    There are some weird behaviour wen starting multiple instances of AFL when
    we add an extra sync() in its run_target loop.  The symptons are that only
    one instance can be started, the other complaining that there was no
    instrumentation output (the afl area was read as all 0).
    
    Try to work-around what ever is causing this by using different offsets
    when mmap'ing /dev/afl in the different processes so that nobody can think
    pages in the multiple afl-fuzz are the same (as they were all mmap()ing
    with MAP_SHARED from offset zero).  A bit far fetched but heh, it doesn't
    hurt anyway!
    
    Signed-off-by: Quentin Casasnovas <quentin.casasnovas@oracle.com>

--- drivers/afl/afl.h
+++ drivers/afl/afl.h
@@ -12,6 +12,7 @@ struct afl_area {
 	struct hlist_node         hlist;
 	struct kref               kref;
 	const struct task_struct* task;
+	unsigned long		  offset; /* fake offset within /dev/afl */
 	unsigned short            prev_location;
 	u8*                       area;
 };
@@ -22,6 +23,7 @@ extern void __afl_maybe_log(void);
 
 # define AFL_CTL_ASSOC_AREA (42)
 # define AFL_CTL_DISASSOC_AREA (43)
+# define AFL_CTL_GET_MMAP_OFFSET (44)
 
 # define AFL_AREA_SIZE_POW2 16
 # define AFL_AREA_SIZE (1 << AFL_AREA_SIZE_POW2)
